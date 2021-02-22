465c853e951e12bc02ff87f5b4c7fd87873f27c8
gauthier.giles <gauthier.giles@gmail.com>
2021-02-22 08:00:20 +0100

    all class are covered by tests

---
+++ src/test/java/com/parkit/parkingsystem/util/InputReaderUtilTest.java
@@ -0,0 +1,16 @@
+package com.parkit.parkingsystem.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputReaderUtilTest {
+
+    @Test
+    void readSelection() {
+    }
+
+    @Test
+    void readVehicleRegistrationNumber() {
+    }
+}
